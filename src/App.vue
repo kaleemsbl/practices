f3f34ae15ed9c2f2c5d4fb3c7c7c556365ee5c37
kaleem <kaleemkhan05071995@gmail.com>
2023-05-15 23:21:43 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+ 
+  <Home kaleem="kaleem choudhary"/>
+</template>
+<script>
+import Home from './components/Home.vue'
+export default{
+    name: 'App',
+    components:{
+        Home,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    <h1 v-if="show">This is the first component</h1>
+    <h2>{{ kaleem }}</h2>
+    </div>
+    <button v-on:click="hello">toggle</button>
+</template>
+<script>
+export default{
+    name: 'Home',
+    props:{
+        kaleem: String
+    },
+    data()
+        {
+             return{show:false}
+        },
+        methods:{
+            hello(){
+            this.show=!this.show
+        }
+        }
+
+}
+</script>
+
+<style scoped>
+button {
+  margin: 25px 5px 20px 20px;
+}
+
+</style>
